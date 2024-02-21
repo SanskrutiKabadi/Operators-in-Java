e8e48224d8e2b0dc04f1be94c19cf638399a3fd3
Sanskrutikabadi <sanskrutikabadi734@gmail.com>
2024-02-21 08:41:02 +0530

    Java Operators

---
+++ Operators5.java
@@ -0,0 +1,11 @@
+class Operators5{
+        public static void main(String[] args){
+               
+                float A=10f;
+                float B=20f;
+                System.out.println("A = "+A);
+		System.out.println("B = "+B);
+
+        }
+
+}
