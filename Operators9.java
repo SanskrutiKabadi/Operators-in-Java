bdb29d204b1aa92fbf97bb59740bb74eb15144a2
Sanskrutikabadi <sanskrutikabadi734@gmail.com>
2024-02-22 08:18:51 +0530

    java Operators

---
+++ Operators9.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Operators9{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+                if(A>B){
+                	System.out.println("Output = "+A);
+		}
+		else{
+			 System.out.println("Output = "+B);
+		}
+        }
+
+}
