ac4d38646333214d038c503cc2184de2599e46c5
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-01 21:00:18 +0530

    Update Operators20.java

--- Operators20.java
+++ Operators20.java
@@ -8,7 +8,7 @@ class Operators20{
                 A=Sc.nextInt();
                 System.out.println("enter second number :");
                 B=Sc.nextInt();
-
+                // or operator
                 Ans=A>B || B>A;
                 System.out.println("Ans = "+Ans);
 
