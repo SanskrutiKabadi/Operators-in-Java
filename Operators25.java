d73131145118eab654bb85c448ffc5164449be3d
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-08 23:35:56 +0530

    Java Operators

---
+++ Operators25.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Operators25{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+                System.out.println((A%2==0 && B%2!=0) ? A:B );
+
+
+        }
+
+}
