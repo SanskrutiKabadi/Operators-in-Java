6c2d11cf593a0111d98295dc9c3d299411adc4f3
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-09 08:37:47 +0530

    Java Operators

---
+++ Operators29.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Operators29{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+		System.out.println( A>>2);
+                System.out.println( A>>>2);
+		System.out.println(B>>>2);
+
+
+        }
+
+}
